70fdf1674b79889b3d99d6741f52564429fce68d
Harvey <shihaoyu2008@gmail.com>
2020-02-09 08:50:11 +0000

    backup all to github

---
+++ sss.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/bin/python /usr/bin/ssserver -p 4435 -k passw0rd -m aes-256-cfb --user nobody -d start
---
+++ u2r1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+udp2raw/udp2raw_amd64 -s -l0.0.0.0:23196 -r 127.0.0.1:22196 -a -kp@ssw0rd --raw-mode icmp
